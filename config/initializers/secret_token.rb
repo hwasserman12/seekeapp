573075cd50647c2f9dc4864529af1cafc1cb542e
Hannah Wasserman <hwasserman12@gmail.com>
2013-10-15 11:45:05 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seekeapp::Application.config.secret_token = 'eab30ee31ff28a57aa3c6aee105ae8ac1c2d1b38491e2bde6c8110823b61a0cc3076c1c1f728d4ac63da54261dc9a00663a975f68e158dca3ecad2d44df269b4'
